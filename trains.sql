608dae0c53d9e7847e93ddc9e36d59966501dc43
alex <livalex11@gmail.com>
2014-09-01 20:42:35 +0300

    added dump db

--- trains.sql
+++ trains.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Авг 15 2014 г., 21:41
+-- Время создания: Сен 01 2014 г., 21:41
 -- Версия сервера: 5.5.35-log
 -- Версия PHP: 5.3.27
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- База данных: `trains`
 --
-CREATE DATABASE IF NOT EXISTS `trains` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `trains`;
 
 -- --------------------------------------------------------
 
@@ -34,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `cars` (
   `places` int(11) NOT NULL COMMENT 'кол-во мест в вагоне',
   `price` int(11) NOT NULL COMMENT 'цена за 1 км',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf32 AUTO_INCREMENT=3 ;
 
 --
 -- Дамп данных таблицы `cars`
@@ -43,6 +41,25 @@ CREATE TABLE IF NOT EXISTS `cars` (
 INSERT INTO `cars` (`id`, `name`, `places`, `price`) VALUES
 (1, 'плацкарт', 50, 50),
 (2, 'купе', 20, 100);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL COMMENT 'ФИО заказчика',
+  `price` int(10) unsigned NOT NULL,
+  `id_route` int(10) unsigned NOT NULL,
+  `id_stationFrom` int(11) NOT NULL,
+  `id_stationTo` int(11) NOT NULL,
+  `car` int(10) unsigned NOT NULL,
+  `place` int(10) unsigned NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf32 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -56,15 +73,36 @@ CREATE TABLE IF NOT EXISTS `parts` (
   `station2` int(11) NOT NULL,
   `length` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=27 ;
 
 --
 -- Дамп данных таблицы `parts`
 --
 
 INSERT INTO `parts` (`id`, `station1`, `station2`, `length`) VALUES
-(1, 13, 15, 11),
-(2, 14, 15, 5);
+(3, 19, 20, 5),
+(4, 19, 21, 8),
+(5, 19, 22, 14),
+(7, 19, 23, 8),
+(8, 19, 24, 4),
+(9, 20, 19, 8),
+(10, 20, 21, 4),
+(11, 20, 22, 14),
+(12, 20, 23, 5),
+(13, 21, 19, 14),
+(14, 21, 20, 5),
+(15, 21, 22, 4),
+(16, 21, 25, 4),
+(17, 24, 19, 5),
+(18, 24, 20, 5),
+(19, 24, 22, 14),
+(20, 24, 27, 4),
+(21, 24, 28, 14),
+(22, 25, 28, 14),
+(23, 25, 30, 8),
+(24, 28, 31, 14),
+(25, 31, 32, 4),
+(26, 22, 25, 8);
 
 -- --------------------------------------------------------
 
@@ -80,8 +118,17 @@ CREATE TABLE IF NOT EXISTS `routes` (
   `cars` text NOT NULL,
   `timeFrom` time NOT NULL COMMENT 'время отбытия из начальной станции',
   `timeTo` time NOT NULL COMMENT 'время прибытия в конечную станцию',
+  `date` date NOT NULL COMMENT 'дата поездки/билета',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf32 AUTO_INCREMENT=4 ;
+
+--
+-- Дамп данных таблицы `routes`
+--
+
+INSERT INTO `routes` (`id`, `stFrom`, `stTo`, `message`, `cars`, `timeFrom`, `timeTo`, `date`) VALUES
+(1, 19, 25, 'ежедневно без выходных', '1,1,1,1,2,2,2,2', '16:00:00', '02:00:00', '2014-09-04'),
+(3, 19, 25, 'ежедневно без выходных', '1,1,2', '07:00:00', '16:00:00', '2014-09-07');
 
 -- --------------------------------------------------------
 
@@ -90,11 +137,37 @@ CREATE TABLE IF NOT EXISTS `routes` (
 --
 
 CREATE TABLE IF NOT EXISTS `routes_parts` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `id_route` int(10) unsigned NOT NULL,
   `id_part` int(10) unsigned NOT NULL,
   `timeFrom` time NOT NULL,
-  `timeTo` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `timeTo` time NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf32 AUTO_INCREMENT=29 ;
+
+--
+-- Дамп данных таблицы `routes_parts`
+--
+
+INSERT INTO `routes_parts` (`id`, `id_route`, `id_part`, `timeFrom`, `timeTo`) VALUES
+(6, 15, 1, '00:00:00', '01:00:00'),
+(7, 15, 2, '01:02:00', '03:00:00'),
+(8, 15, 1, '00:00:00', '01:00:00'),
+(9, 15, 2, '01:02:00', '03:00:00'),
+(10, 15, 1, '03:03:00', '05:00:00'),
+(11, 15, 1, '00:00:00', '00:00:00'),
+(17, 2, 4, '16:00:00', '18:00:00'),
+(18, 2, 16, '18:05:00', '21:00:00'),
+(19, 2, 22, '21:05:00', '02:00:00'),
+(20, 2, 24, '02:10:00', '04:00:00'),
+(21, 2, 25, '04:03:00', '06:00:00'),
+(22, 3, 3, '07:00:00', '08:30:00'),
+(23, 3, 11, '08:40:00', '10:00:00'),
+(24, 3, 26, '10:05:00', '13:00:00'),
+(25, 3, 22, '13:10:00', '16:00:00'),
+(26, 1, 4, '16:00:00', '18:00:00'),
+(27, 1, 16, '18:05:00', '21:00:00'),
+(28, 1, 22, '21:05:00', '02:00:00');
 
 -- --------------------------------------------------------
 
@@ -107,21 +180,27 @@ CREATE TABLE IF NOT EXISTS `Stations` (
   `city` varchar(128) NOT NULL,
   `name` varchar(128) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=33 ;
 
 --
 -- Дамп данных таблицы `Stations`
 --
 
 INSERT INTO `Stations` (`id`, `city`, `name`) VALUES
-(11, 'Mariupol', '1'),
-(12, 'Mariupol', '2sdlkfjghsklj'),
-(13, 'город 1', 'станция 1'),
-(14, 'город 1', 'станция 2'),
-(15, 'город 2', 'станция 1'),
-(16, 'город 3', 'станция 1'),
-(17, 'город 3', 'станция 2'),
-(18, 'город 3', 'станция 3');
+(19, 'г1', 'с1'),
+(20, 'г1', 'с2'),
+(21, 'г2', 'с1'),
+(22, 'г2', 'с2'),
+(23, 'г2', 'с3'),
+(24, 'г3', 'с1'),
+(25, 'г3', 'с2'),
+(26, 'г3', 'с3'),
+(27, 'г3', 'с4'),
+(28, 'г4', 'с1'),
+(29, 'г4', 'с2'),
+(30, 'г4', 'с3'),
+(31, 'г5', 'с1'),
+(32, 'г5', 'с2');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
