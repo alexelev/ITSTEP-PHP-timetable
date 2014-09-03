cbb455a1777dbfe838cd1ff7b9e30b478db06865
alex <livalex11@gmail.com>
2014-09-03 20:33:32 +0300

    added some route view

--- index.php
+++ index.php
@@ -12,4 +12,47 @@
     print_r($_POST);
     echo('</pre>');
 
+    if(isset($_POST['stationFrom']) && isset($_POST['stationTo'])){
+	    $query = "SELECT `r`.*, `rp1`.`id` as `id_rp1`, `rp2`.`id` as `id_rp2`
+				FROM `routes` AS `r`
+
+				LEFT JOIN `routes_parts` AS `rp1` ON `rp1`.`id_route` = `r`.`id`
+				LEFT JOIN `parts` AS `p1` ON `p1`.`id` = `rp1`.`id_part`
+
+				LEFT JOIN `routes_parts` AS `rp2` ON `rp2`.`id_route` = `r`.`id`
+				LEFT JOIN `parts` AS `p2` ON `p2`.`id` = `rp2`.`id_part`
+				WHERE `p1`.`station1` = {$_POST['stationFrom']} AND `p2`.`station2` = {$_POST['stationTo']} 
+				AND `rp1`.`id` <= `rp2`.`id`";
+
+		echo $query.'<br/>';
+		
+		$result = mysql_query($query) or die(mysql_error());
+
+		$prices = array();
+		$query = "SELECT `price` FROM `cars` ORDER BY `id`";
+		$result = mysql_query($query) or die(mysql_error());
+		$price[1] = current(mysql_fetch_assoc($result));
+		$price[2] = current(mysql_fetch_assoc($result));
+		
+		$template['r'] = array();
+		while($row = mysql_fetch_assoc($result)){			
+			$id1 = $row['id_rp1'];
+			$id2 = $row['id_rp2'];
+			$id_r = $row['id'];
+			$query = "SELECT SUM(`p`.`length`) FROM `routes_parts` as `rp` 
+						LEFT JOIN `parts` as `p` ON `p`.`id` = `rp`.`id_part`
+						WHERE `rp`.`id_route` = {$id_r} AND (`rp`.`id` BETWEEN {$id1} AND {$id2})";
+			echo $query.'<br/>';
+			$r = mysql_query($query) or die(mysql_error());
+			$row['length'] = current(mysql_fetch_assoc($r));
+			$row['typeCar1exist'] = (strpos($row['cars'], '1') !== false ? true : false);
+			$row['typeCar2exist'] = (strpos($row['cars'], '2') !== false ? true : false);
+			$template['r'][] = $row;
+		}
+	}
+
+	echo('<pre>');
+    var_dump($template['r']);
+    echo('</pre>');
+
 	include ABSPATH.'/views/index.php';
--- views/index.php
+++ views/index.php
@@ -6,7 +6,7 @@
 </head>
     <body>
         <form action="" method="POST">
-            <select name="stationsFrom" id="">
+            <select name="stationFrom" id="">
                 <option value="-1">Choose the station</option>
                 <?php foreach ($template['list'] as $key => $station) { ?>
                     <option value="<?=$station['id']?>"><?=$station['city'].' -> '.$station['name']?></option>
@@ -20,5 +20,34 @@
             </select>
             <input type="submit" value="NEXT">
         </form>
+	    <table>
+			<thead>
+				<tr>
+					<th>id</th>
+					<th>stFrom</th>
+					<th>stTo</th>
+					<th>message</th>
+					<th>timeFrom</th>
+					<th>timeTo</th>
+					<th>Length</th>
+					<th colspan="2"></th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach($template['r'] as $item){ ?>
+					<tr>
+						<td><?= $item['id'] ?></td>
+						<td><?= $item['stFrom'] ?></td>
+						<td><?= $item['stTo'] ?></td>
+						<td><?= $item['message'] ?></td>
+						<td><?= $item['timeFrom'] ?></td>
+						<td><?= $item['timeTo'] ?></td>
+						<td><?= $item['length'] ?></td>
+						<td><a href="/admin/stations.php?action=edit&id=<?=$item['id']?>">edit</a></td>
+						<td><a href="/admin/stations.php?action=delete&id=<?=$item['id']?>">delete</a></td>
+					</tr>
+				<?} ?>
+			</tbody>		
+		</table>
     </body>
 </html>
